bc56f557a0934a7d514d66f60e2525c9ceed195c
Tri <logger1606@gmail.com>
2021-04-20 20:43:35 +0200

    intro

--- introduction/set_joint_commands.py
+++ introduction/set_joint_commands.py
@@ -23,6 +23,7 @@ class MyAgent(SparkAgent):
         # YOUR CODE HERE
         action.stiffness['LShoulderPitch'] = 0
         action.speed['HeadYaw'] = 0.1
+        #print('LShoulderPitch: ' + str(action.stiffness['LShoulderPitch']) + 'HeadYaw: ' + str(action.speed['HeadYaw']))
         return action
 
 if '__main__' == __name__:
