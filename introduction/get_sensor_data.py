3a2834c8c7caec7053cd1572dccbd5ab9d66bff6
Tri <logger1606@gmail.com>
2021-04-22 14:53:01 +0200

    intro1

--- introduction/get_sensor_data.py
+++ introduction/get_sensor_data.py
@@ -24,7 +24,7 @@ class MyAgent(SparkAgent):
         angle = perception.joint['HeadYaw']
         temperature = perception.joint_temperature['HeadYaw']
 
-        print('HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature))
+        print 'HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature)
         return super(MyAgent, self).think(perception)
 
 if '__main__' == __name__:
